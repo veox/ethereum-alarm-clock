3c96ab5360a650d48ebaae17066784851151428c
Noel Maersk <veox@users.noreply.github.com>
2017-04-19 16:20:23 +0300

    SQ

--- bin/install_solc.sh
+++ bin/install_solc.sh
@@ -24,6 +24,7 @@ else
     # cached version present, just install package dependencies
     cd solc-versions
     cd solidity-0.3.6
+    ls -al
     ./scripts/install_deps.sh
     echo "Geth already installed at $PWD/solc/solc-0.3.6"
 fi
