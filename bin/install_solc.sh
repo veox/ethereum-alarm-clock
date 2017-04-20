1bb80b985cc055d4c1bcd456072f642ef22aa5d1
Noel Maersk <veox@users.noreply.github.com>
2017-04-20 18:22:05 +0300

    travis: pilfer Populus for .travis.yml and bin/install_solc.sh.
    The latter copy-pasted over, the former - only snippets, not using
    `tox` yet.

--- bin/install_solc.sh
+++ bin/install_solc.sh
@@ -6,38 +6,11 @@
 set -e
 set -u
 
-if [ ! -e $SOLC_BINARY ] ; then
-    # cache not present (first run or cleared), perform full installation procedure
-
-    # pwd: .
-    wget "https://github.com/ethereum/solidity/archive/v$SOLC_VERSION.tar.gz"
-    install -d solc-versions
-    cd solc-versions
-
-    # pwd: ./solc-versions
-    tar -zxvf ../v$SOLC_VERSION.tar.gz
-    cd solidity-$SOLC_VERSION
-
-    # pwd: ./solc-versions/solidity-$SOLC_VERSION
-    ./scripts/install_deps.sh
-
-    mkdir -p build
-    cd build
-
-    # pwd: ./solc-versions/solidity-$SOLC_VERSION/build
-    cmake .. && make
-
-    ln -fs $PWD/solc/solc $SOLC_BINARY
-    chmod +x $SOLC_BINARY
-
-    echo "Solidity compiler installed at $SOLC_BINARY"
-else
-    # cached version present, just install package dependencies
-    echo "Solidity compiler already installed at $SOLC_BINARY"
-    cd solc-versions/solidity-$SOLC_VERSION
-    ./scripts/install_deps.sh
-fi
-
-# DEBUG
-ls -l $SOLC_BINARY
-file $SOLC_BINARY
+mkdir -p solc-versions/solc-$SOLC_VERSION
+cd solc-versions/solc-$SOLC_VERSION
+git clone --recurse-submodules --branch v$SOLC_VERSION --depth 50 https://github.com/ethereum/solidity.git
+./solidity/scripts/install_deps.sh
+wget https://github.com/ethereum/solidity/releases/download/v$SOLC_VERSION/solidity-ubuntu-trusty.zip
+unzip solidity-ubuntu-trusty.zip
+echo "Solidity installed at $TRAVIS_BUILD_DIR/solc-versions/solc-$SOLC_VERSION/solc"
+tree $TRAVIS_BUILD_DIR/solc-versions/solc-$SOLC_VERSION
